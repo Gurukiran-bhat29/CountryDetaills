a6d632d1f4de9b4abb91038d40fae4f2b6b280c0
Gurukiran <gurukiran.bhat@niveussolutions.com>
2022-09-03 15:19:26 +0530

    new changes

--- src/SelectedCountry.js
+++ src/SelectedCountry.js
@@ -20,32 +20,38 @@ function selectedCountry({ details, onClickBack }) {
       </div>
       <div className='flexOne'>
         <label className='selectedName'>{details.name}</label>
-        <label>
-          <span className='fontBold'>Native Name:</span> {details.nativeName}
-        </label>
-        <label>
-          <span className='fontBold'>Population:</span> {details.population}
-        </label>
-        <label>
-          <span className='fontBold'>Region:</span> {details.region}
-        </label>
-        <label>
-          <span className='fontBold'>Sub Region:</span> {details.subregion}
-        </label>
-        <label>
-          <span className='fontBold'>Capital:</span> {details.capital}
-        </label>
-        <label>
-          <span className='fontBold'>Top Level Domain:</span> {details.tld}
-        </label>
-        <label>
-          <span className='fontBold'>Currencies:</span>
-          {details.currencies.map((currency) => currency)}
-        </label>
-        <label>
-          <span className='fontBold'>Languages:</span>
-          {details.languages.map((language) => language)}
-        </label>
+        <div className='displayFlex'>
+          <div className='flexOne'>
+            <label className='displayBlock'>
+              <span className='fontBold'>Native Name:</span> {details.nativeName}
+            </label>
+            <label className='displayBlock'>
+              <span className='fontBold'>Population:</span> {details.population}
+            </label>
+            <label className='displayBlock'>
+              <span className='fontBold'>Region:</span> {details.region}
+            </label>
+            <label className='displayBlock'>
+              <span className='fontBold'>Sub Region:</span> {details.subregion}
+            </label>
+          </div>
+          <div className='flexOne'>
+            <label className='displayBlock'>
+              <span className='fontBold'>Capital:</span> {details.capital}
+            </label>
+            <label className='displayBlock'>
+              <span className='fontBold'>Top Level Domain:</span> {details.tld}
+            </label>
+            <label className='displayBlock'>
+              <span className='fontBold'>Currencies:</span>
+              {details.currencies.map((currency) => `${currency} `)}
+            </label>
+            <label className='displayBlock'>
+              <span className='fontBold'>Languages:</span>
+              {details.languages.map((language) => `${language} `)}
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   )
