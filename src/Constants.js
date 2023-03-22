d6bfc874943a34866279f160fca7477c36d1df81
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-03-22 13:29:29 +0530

    Code optimization

---
+++ src/Constants.js
@@ -0,0 +1,7 @@
+export const filteredCountry = [
+  { country: 'Africa', id: 'africa' },
+  { country: 'America', id: 'america' },
+  { country: 'Asia', id: 'asia' },
+  { country: 'Europe', id: 'europe' },
+  { country: 'Oceania', id: 'oceania' }
+];
