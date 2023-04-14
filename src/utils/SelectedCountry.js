b333ea9abbcdfc15aa40cc3a2cb8da69df9b5e6d
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-04-14 19:58:06 +0530

    responsive changes

--- src/utils/SelectedCountry.js
+++ src/utils/SelectedCountry.js
@@ -5,64 +5,62 @@ function selectedCountry({ details, onClickBack }) {
   const languageLength = details.languages.length - 1;
   return (
     <div className="selectedCountry">
-      <div className='flexOne'>
-        <div className='selectedFlagContainer'>
-          <img
-            className='selectedFlag'
-            src={details.flag}
-            alt='SelectedCountry'
-          />
-        </div>
-        <button
-          className='back'
-          onClick={() => onClickBack()}
-        >
-          Back
-        </button>
-      </div>
-      <div className='flexOne'>
-        <label className='selectedName'>{details.name}</label>
-        <div className='displayFlex'>
-          <div className='flexOne'>
-            <label className='displayBlock'>
-              <span className='fontBold'>Native Name:</span> {details.nativeName}
-            </label>
-            <label className='displayBlock'>
-              <span className='fontBold'>Population:</span> {details.population}
-            </label>
-            <label className='displayBlock'>
-              <span className='fontBold'>Region:</span> {details.region}
-            </label>
-            <label className='displayBlock'>
-              <span className='fontBold'>Sub Region:</span> {details.subregion}
-            </label>
-          </div>
-          <div className='flexOne'>
-            <label className='displayBlock'>
-              <span className='fontBold'>Capital:</span> {details.capital}
-            </label>
-            <label className='displayBlock'>
-              <span className='fontBold'>Top Level Domain:</span> {details.tld}
-            </label>
-            <label className='displayBlock'>
-              <span className='fontBold'>Currencies:</span>
-              {details.currencies && details.currencies.map((currency, index) =>
-                currencyLength !== index
-                  ? ` ${currency},`
-                  : ` ${currency}`
-              )}
-            </label>
-            <label className='displayBlock'>
-              <span className='fontBold'>Languages:</span>
-              {details.currencies && details.languages.map((language, index) =>
-                languageLength !== index
-                  ? ` ${language},`
-                  : ` ${language}`
-              )}
-            </label>
+      <div className='selectedInnerContainer'>
+        <img
+          className='flexOne selectedFlag'
+          src={details.flag}
+          alt='SelectedCountry'
+        />
+        <div className='flexOne detailsContainer'>
+          <label className='selectedName'>{details.name}</label>
+          <div className='displayFlex'>
+            <div className='flexOne'>
+              <label className='displayBlock'>
+                <span className='fontBold'>Native Name:</span> {details.nativeName}
+              </label>
+              <label className='displayBlock'>
+                <span className='fontBold'>Population:</span> {details.population}
+              </label>
+              <label className='displayBlock'>
+                <span className='fontBold'>Region:</span> {details.region}
+              </label>
+              <label className='displayBlock'>
+                <span className='fontBold'>Sub Region:</span> {details.subregion}
+              </label>
+            </div>
+            <div className='flexOne'>
+              <label className='displayBlock'>
+                <span className='fontBold'>Capital:</span> {details.capital}
+              </label>
+              <label className='displayBlock'>
+                <span className='fontBold'>Top Level Domain:</span> {details.tld}
+              </label>
+              <label className='displayBlock'>
+                <span className='fontBold'>Currencies:</span>
+                {details.currencies && details.currencies.map((currency, index) =>
+                  currencyLength !== index
+                    ? ` ${currency},`
+                    : ` ${currency}`
+                )}
+              </label>
+              <label className='displayBlock'>
+                <span className='fontBold'>Languages:</span>
+                {details.currencies && details.languages.map((language, index) =>
+                  languageLength !== index
+                    ? ` ${language},`
+                    : ` ${language}`
+                )}
+              </label>
+            </div>
           </div>
         </div>
       </div>
+      <button
+        className='back'
+        onClick={() => onClickBack()}
+      >
+        Back
+      </button>
     </div>
   )
 }
